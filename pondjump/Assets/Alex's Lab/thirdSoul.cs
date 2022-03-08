1893b33d9eca7d02d7d1b241dbe3d0f3ef92983d
mtphadnis <91328231+mtphadnis@users.noreply.github.com>
2022-03-08 15:28:19 -0500

    Sounds+UI

--- pondjump/Assets/Alex's Lab/thirdSoul.cs
+++ pondjump/Assets/Alex's Lab/thirdSoul.cs
@@ -512,10 +512,22 @@ public class thirdSoul : MonoBehaviour
 
     public void Paused(InputAction.CallbackContext context)
     {
-        Debug.Log("Pause: " + Pause);
-        if (context.performed) { Pause = true; }
-        else { Pause = false; }
+      Debug.Log("Pause: " + Pause);
+            if (context.started)
+            {
+                Pause = true;
+            }
 
+
+
+            else if (context.canceled)
+            {
+                Pause = false;
+            }
+
+            Cursor.lockState = CursorLockMode.Confined;
+
+ 
     }
 
     void LaunchIndicatorCheck(float active)
