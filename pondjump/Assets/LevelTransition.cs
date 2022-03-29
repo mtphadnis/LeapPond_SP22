853a6393e207b39bb597182684ad0c89dfbba5d1
Xanderwell <ajc2943@gmail.com>
2022-03-29 18:36:35 -0400

    Pt01

---
+++ pondjump/Assets/LevelTransition.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    public string NextLevelName;
+    Scene NextLevel, CurrentScene;
+
+
+    private void Start()
+    {
+        NextLevel = SceneManager.GetSceneByName(NextLevelName);
+
+        CurrentScene = SceneManager.GetActiveScene();
+        Debug(CurrentScene)
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.SetActiveScene(NextLevel);
+        }
+    }
+}
